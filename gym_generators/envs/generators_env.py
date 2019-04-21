cd51c79730a06d420fd876e41ef2cf561ef52389
Daniel Cregg <danielcregg@users.noreply.github.com>
2019-04-21 18:26:11 +0100

    Add Env

--- gym_generators/envs/generators_env.py
+++ gym_generators/envs/generators_env.py
@@ -2,7 +2,7 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-class Generators(gym.Env):
+class GeneratorsEnv(gym.Env):
 	metadata = {'render.modes': ['human']}
 
 
