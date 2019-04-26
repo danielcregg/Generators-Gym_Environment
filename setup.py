af16c3165dae33f2ae15189a0e4d045c5fe6f21f
Daniel Cregg <danielcregg@users.noreply.github.com>
2019-04-26 14:32:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,13 @@ from setuptools import setup
 
 setup(name='gym_generators',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      keywords='environment, agent, rl, openaigym, openai-gym, gym',
+      url='https://github.com/danielcregg/gym-generators',
+      description='Generators environment package for OpenAI Gym',
+      packages=['gym_generators', 'gym_generators.envs'],
+      install_requires=[
+        'gym>=0.9.6',
+        'numpy>=1.15.0',
+        'pandas'
+      ]
 )  
