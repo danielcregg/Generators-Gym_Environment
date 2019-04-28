ac1975a930f49ba1187e7d42db2c66719e9c7de9
danielcregg <daniel.cregg@gmail.com>
2019-04-28 21:35:08 +0100

    Added new empty functions and some clean up

--- gym_generators/envs/generators_env.py
+++ gym_generators/envs/generators_env.py
@@ -10,16 +10,17 @@ class GeneratorsEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     # Set constants
-    N = 10  # N = number of generators
-    M = 24  # M = number of hours in day
-    E = 10  # Emissions scaling factor
+    N = 10      # N = number of generators
+    M = 24      # M = number of hours in day
+    E = 10      # Emissions scaling factor
     Wc = 0.225  # Cost weight used for linear scalarisation
     We = 0.275  # Emissions weight used for linear scalarisation
-    Wp = 0.5  # Power weight used for linear scalarisation
+    Wp = 0.5    # Power weight used for linear scalarisation
+    C = 10E6    # C is the violation constant
 
     # DataFrame created with generator data in the form of list of tuples
     gen_chars = pd.DataFrame(
-        [(150, 470, 786.7988, 38.5397, 0.1524, 450, 0.041, 103.3908, -2.4444, 0.0312, 0.5035, 0.0207, 80, 80),
+        [(150, 470, 786.7988, 38.5397, 0.1524, 450, 0.041, 103.3908, -2.4444,   0.0312, 0.5035, 0.0207, 80, 80),
          (135, 470, 451.3251, 46.1591, 0.1058, 600, 0.036, 103.3908, -2.4444, 0.0312, 0.5035, 0.0207, 80, 80),
          (73, 340, 1049.9977, 40.3965, 0.0280, 320, 0.028, 300.3910, -4.0695, 0.0509, 0.4968, 0.0202, 80, 80),
          (60, 300, 1243.5311, 38.3055, 0.0354, 260, 0.052, 300.3910, -4.0695, 0.0509, 0.4968, 0.0202, 50, 50),
@@ -29,14 +30,13 @@ class GeneratorsEnv(gym.Env):
          (47, 120, 1450.7045, 36.5104, 0.0121, 340, 0.082, 330.0056, -3.9023, 0.0465, 0.5163, 0.0214, 30, 30),
          (20, 80, 1455.6056, 39.5804, 0.1090, 270, 0.098, 350.0056, -3.9524, 0.0465, 0.5475, 0.0234, 30, 30),
          (10, 55, 1469.4026, 40.5407, 0.1295, 380, 0.094, 360.0012, -3.9864, 0.0470, 0.5475, 0.0234, 30, 30)],
-        columns=["p_min_i", "p_max_i", "a_i", "b_i", "c_i", "d_i", "e_i", "alpha_i", "beta_i", "gamma_i", "eta_i",
-                 "delta_i", "ur_i", "dr_i"],
+        columns=["p_min_i", "p_max_i", "a_i", "b_i", "c_i", "d_i", "e_i", "alpha_i", "beta_i", "gamma_i", "eta_i", "delta_i", "ur_i", "dr_i"],
         index=["unit1", "unit2", "unit3", "unit4", "unit5", "unit6", "unit7", "unit8", "unit9", "unit10"])
 
     # B is the matrix of transmission line loss coefficients
     # data in the form of list of tuples
     B = pd.DataFrame(
-        [(0.000049, 0.000014, 0.000015, 0.000015, 0.000016, 0.000017, 0.000017, 0.000018, 0.000019, 0.000020),
+        [(0.000049, 0.000014, 0.000015, 0.000015, 0.000016, 0.000017, 0.000017,  0.000018, 0.000019, 0.000020),
          (0.000014, 0.000045, 0.000016, 0.000016, 0.000017, 0.000015, 0.000015, 0.000016, 0.000018, 0.000018),
          (0.000015, 0.000016, 0.000039, 0.000010, 0.000012, 0.000012, 0.000014, 0.000014, 0.000016, 0.000016),
          (0.000015, 0.000016, 0.000010, 0.000040, 0.000014, 0.000010, 0.000011, 0.000012, 0.000014, 0.000015),
@@ -52,12 +52,9 @@ class GeneratorsEnv(gym.Env):
 
     # data in the form of list of tuples
     hour_power_demand = pd.DataFrame(
-        [1036, 1110, 1258, 1406, 1480, 1628, 1702, 1776, 1924, 2022, 2106, 2150, 2072, 1924,
-         1776, 1554, 1480, 1628, 1776, 1972, 1924, 1628, 1332, 1184],
+        [1036, 1110, 1258, 1406, 1480, 1628, 1702, 1776, 1924, 2022, 2106,      2150, 2072, 1924, 1776, 1554, 1480, 1628, 1776, 1972, 1924, 1628,      1332, 1184],
         columns=["p_d"],
-        index=["hour1", "hour2", "hour3", "hour4", "hour5", "hour6", "hour7", "hour8", "hour9", "hour10", "hour11",
-               "hour12", "hour13", "hour14", "hour15", "hour16", "hour17", "hour18", "hour19", "hour20", "hour21",
-               "hour22", "hour23", "hour24"])
+        index=["hour1", "hour2", "hour3", "hour4", "hour5", "hour6", "hour7", "hour8", "hour9", "hour10", "hour11", "hour12", "hour13", "hour14", "hour15", "hour16", "hour17", "hour18", "hour19", "hour20", "hour21","hour22", "hour23", "hour24"])
 
     hour_power_demand_diff = hour_power_demand.diff().fillna(0.)  # Get diff values and fill NaN with 0.0
     states = hour_power_demand_diff.assign(p_n_m_prev=0.)  # Add new column and set all rows to 0.0
@@ -112,12 +109,17 @@ class GeneratorsEnv(gym.Env):
             global_emissions += self.emissions_function_local(i)
         return global_emissions
     
-    def render(self):
-        print("This is a render of the Generators Environment")
+    def global_penalty_fuction(self):
+        print("This is the Global Penalty Function")
+        return 0
+
+    def constrain_action_space(self):
+        print("This is the restrain action space Function")
+        return self.action_space
+
 
     def step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
-        
         #ur_i = self.gen_chars.loc[self.active_unit, "ur_i"]
         #dr_i = self.gen_chars.loc[self.active_unit, "dr_i"]
         p_min_i = self.gen_chars.loc[self.active_unit, "p_min_i"]
